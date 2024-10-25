9feda7fa7b2cf2b7b9e602af6828f643944ca435
sjbeskur <sam.beskur@gmaile.com>
2024-10-24 23:28:21 -0600

    update opencv

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use std::{
-    error::Error, 
-    fs::{self},
-};
 use opencv::{
-    core, 
-    dnn, 
-    highgui, 
-    imgproc, 
+    core, dnn, highgui, imgproc,
     prelude::{self, *},
-    types, 
-    videoio::{self, VideoCaptureTrait}
+    types,
+    videoio::{self, VideoCaptureTrait},
+};
+use std::{
+    error::Error,
+    fs::{self},
 };
 
 use clap::Parser;
@@ -27,11 +24,10 @@ pub struct Args {
     pub coco: String,
 }
 
-
 fn main() {
     // Do not return Result from main, it prints the Debug
-    // representation of the error. 
-    // Executing it in another function prints the Display version 
+    // representation of the error.
+    // Executing it in another function prints the Display version
     // of the error.
     if let Err(err) = try_main() {
         eprintln!("{}", err);
@@ -43,8 +39,10 @@ fn try_main() -> Result<(), Box<dyn Error>> {
     // argument parsing
     let args = Args::parse();
 
-    // initialize video capture 
-    let mut video_capture = videoio::VideoCapture::from_file(&args.file, videoio::CAP_ANY)?;
+    println!("file: {}", args.file);
+    // initialize video capture
+    let mut video_capture = videoio::VideoCapture::from_file(&args.file, videoio::CAP_FFMPEG)?;
+    //let mut video_capture = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
 
     // initialize neural network
     let mut net = dnn::read_net_from_darknet(&args.config, &args.weights)?;
@@ -56,17 +54,21 @@ fn try_main() -> Result<(), Box<dyn Error>> {
 
     // run application
     run(&mut video_capture, &mut net, &classes)?;
-    
+
     Ok(())
 }
 
-fn run(video_capture: &mut videoio::VideoCapture, net: &mut dnn::Net, classes: &types::VectorOfString) -> Result<(), Box<dyn Error>> {
+fn run(
+    video_capture: &mut videoio::VideoCapture,
+    net: &mut dnn::Net,
+    classes: &types::VectorOfString,
+) -> Result<(), Box<dyn Error>> {
     // preallocate images
     let mut img = prelude::Mat::default();
     let mut blob = prelude::Mat::default();
 
     // define parameters for the network
-    let conf_threshold= 0.5_f32;
+    let conf_threshold = 0.5_f32;
     let nms_threshold = 0.4_f32;
     let inp_width = 416;
     let inp_height = 416;
@@ -81,27 +83,27 @@ fn run(video_capture: &mut videoio::VideoCapture, net: &mut dnn::Net, classes: &
         // break loop if video capture has no more frames
         if !video_capture.grab()? {
             println!("Video processing finished");
-            break
+            break;
         }
 
         // create a 4D blob from image
         dnn::blob_from_image_to(
-            &img, 
-            &mut blob, 
-            1./255., 
-            core::Size::new(inp_width, inp_height), 
-            core::Scalar::new(0.,0.,0., 0.), 
-            false, 
-            false, 
-            core::CV_32F
+            &img,
+            &mut blob,
+            1. / 255.,
+            core::Size::new(inp_width, inp_height),
+            core::Scalar::new(0., 0., 0., 0.),
+            false,
+            false,
+            core::CV_32F,
         )?;
 
         // get the names of output layer for bbox naming
         let names = get_output_names(&net)?;
 
         // forward propagation through the network
-        let mut net_output = types::VectorOfMat::new();
-        net.set_input(&blob, "", 1.0, core::Scalar::new(0.,0.,0., 0.))?;
+        let mut net_output = core::Vector::<Mat>::new();
+        net.set_input(&blob, "", 1.0, core::Scalar::new(0., 0., 0., 0.))?;
         net.forward(&mut net_output, &names)?;
 
         // scan through all bounding boxes and keep only the ones with high confidence
@@ -110,29 +112,39 @@ fn run(video_capture: &mut videoio::VideoCapture, net: &mut dnn::Net, classes: &
         let mut boxes = types::VectorOfRect::new();
 
         // remove the bounding boxes with low confidence using non-maxima suppression
-        for (i, matrix) in net_output.iter().enumerate() {          
+        for (i, matrix) in net_output.iter().enumerate() {
             for j in 0..matrix.rows() {
-                let data = matrix.at_row::<f32>(j as i32)?; 
-                let scores = net_output.get(i)?.row(j)?.col_range(&core::Range::new(5, net_output.get(i)?.cols())?)?;
+                let data = matrix.at_row::<f32>(j as i32)?;
+                // let scores = net_output
+                //     .get(i)?
+                //     .row(j)?
+                //     .col_range(&core::Range::new(5, net_output.get(i)?.cols())?)?;
+
+                //let scores = net_output.get(i)?.row(j)?.col_range(&core::Range::new(5, net_output.get(i)?.cols())?)?;                
+
+                let output = net_output.get(i)?;
+                let output2 = output.row(j)?;
+                let scores = output2.col_range(&core::Range::new(5, output.cols())?)?;
+
                 let mut class_id_point = core::Point::default();
                 let mut confidence = 0_f64;
 
                 core::min_max_loc(
-                    &scores, 
-                    Some(&mut 0.), 
-                    Some(&mut confidence), 
-                    Some(&mut core::Point::new(0,0)), 
+                    &scores,
+                    Some(&mut 0.),
+                    Some(&mut confidence),
+                    Some(&mut core::Point::new(0, 0)),
                     Some(&mut class_id_point),
-                    &core::no_array()
+                    &core::no_array(),
                 )?;
 
                 if confidence > conf_threshold as f64 {
-                    let center_x = (data[0] *  img_width as f32) as i32;
+                    let center_x = (data[0] * img_width as f32) as i32;
                     let center_y = (data[1] * img_height as f32) as i32;
-                    let width = (data[2] * img_width as f32) as i32;                 // w
-                    let height = (data[3] * img_height as f32) as i32;               // h
-                    let left = center_x - (width / 2);                               // x
-                    let top = center_y - (height / 2);                               // y
+                    let width = (data[2] * img_width as f32) as i32; // w
+                    let height = (data[3] * img_height as f32) as i32; // h
+                    let left = center_x - (width / 2); // x
+                    let top = center_y - (height / 2); // y
 
                     class_ids.push(class_id_point.x);
                     confidences.push(confidence as f32);
@@ -143,29 +155,44 @@ fn run(video_capture: &mut videoio::VideoCapture, net: &mut dnn::Net, classes: &
 
         // perform non maximum suppression to remove redundant overlapping boxes with lower confidences
         let mut indices = types::VectorOfi32::new();
-        dnn::nms_boxes(&boxes, &confidences, conf_threshold, nms_threshold, &mut indices, 1., 0)?;
+        dnn::nms_boxes(
+            &boxes,
+            &confidences,
+            conf_threshold,
+            nms_threshold,
+            &mut indices,
+            1.,
+            0,
+        )?;
 
         for num in indices.iter() {
             // get bounding box and associated class
-            let bbox = boxes.get(num as usize)?;     
+            let bbox = boxes.get(num as usize)?;
             let label = classes.get(class_ids.get(num as usize)? as usize)?;
-            
+
             // draw predicted bounding box with associated class
-            imgproc::rectangle(&mut img, bbox, core::Scalar::new(255., 18., 50., 0.0), 2, imgproc::LINE_8, 0)?;
+            imgproc::rectangle(
+                &mut img,
+                bbox,
+                core::Scalar::new(255., 18., 50., 0.0),
+                2,
+                imgproc::LINE_8,
+                0,
+            )?;
             imgproc::put_text(
-                &mut img, 
-                &label, 
-                core::Point::new(bbox.x, bbox.y), 
-                imgproc::FONT_HERSHEY_SIMPLEX, 
-                0.75, 
-                core::Scalar::new(0., 0., 0., 0.), 
-                1, 
-                imgproc::LINE_8, 
-                false
+                &mut img,
+                &label,
+                core::Point::new(bbox.x, bbox.y),
+                imgproc::FONT_HERSHEY_SIMPLEX,
+                0.75,
+                core::Scalar::new(255., 255., 255., 0.),
+                2,
+                imgproc::LINE_8,
+                false,
             )?;
         }
 
-        // show frame 
+        // show frame
         highgui::imshow("image", &img)?;
     }
 
@@ -176,18 +203,25 @@ fn get_output_names(net: &dnn::Net) -> Result<types::VectorOfString, Box<dyn Err
     let layers = net.get_unconnected_out_layers()?;
     let layer_names = net.get_layer_names()?;
 
-    Ok(
-        layers.iter().enumerate().fold(types::VectorOfString::new(), |mut names, (i, _)| {
-            names.insert(i, &layer_names.get(
-                (layers.get(i).unwrap() - 1) as usize).expect("No such value.")
-            ).expect("Failed inserting value.");
+    Ok(layers
+        .iter()
+        .enumerate()
+        .fold(types::VectorOfString::new(), |mut names, (i, _)| {
             names
-        })
-    )
+                .insert(
+                    i,
+                    &layer_names
+                        .get((layers.get(i).unwrap() - 1) as usize)
+                        .expect("No such value."),
+                )
+                .expect("Failed inserting value.");
+            names
+        }))
 }
 
 fn read_file(file_name: &str) -> Result<types::VectorOfString, Box<dyn Error>> {
-    Ok(
-        fs::read_to_string(file_name)?.split_whitespace().map(|name| name.into()).collect()
-    )
+    Ok(fs::read_to_string(file_name)?
+        .split_whitespace()
+        .map(|name| name.into())
+        .collect())
 }
